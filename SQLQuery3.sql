869c49e141788a7d5a6c1b8428d6c4bbd2d17d7a
Jeferson <93160555+Jeferson-Oliveiraa@users.noreply.github.com>
2024-05-04 11:39:23 -0300

    Completando desafio banco de dados

---
+++ SQLQuery3.sql
@@ -0,0 +1,10 @@
+-- 1
+Select Nome, Ano FROM Filmes
+-- 2
+Select * FROM Filmes order by Ano
+-- 3
+Select Nome, Ano, Duracao From Filmes Where Id = 28
+-- 4
+Select * FROM Filmes Where Ano = 1997
+-- 5
+Select * FROM Filmes Where Ano>2000 
