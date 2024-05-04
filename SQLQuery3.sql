4571305fb95933c69374e93e478c60d37938f574
Jeferson <93160555+Jeferson-Oliveiraa@users.noreply.github.com>
2024-05-04 14:26:17 -0300

    Atualizando código

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -8,3 +8,12 @@ Select Nome, Ano, Duracao From Filmes Where Id = 28
 Select * FROM Filmes Where Ano = 1997
 -- 5
 Select * FROM Filmes Where Ano>2000 
+-- 6
+Select * From Filmes Where Duracao>100 and Duracao<150 order by Duracao asc
+--7
+Select Ano,count(*) Quantidade From Filmes Group by Ano order by Quantidade desc
+--8
+Select * From Atores where genero = 'M'
+--9
+Select * from Atores where genero = 'F' order by PrimeiroNome asc
+--10
